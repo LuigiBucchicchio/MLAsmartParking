9e27ba64a1d9579f651b946dfecad8f4234362d0
LuigiBucchicchio <luigi.bucchicchio@studenti.unicam.it>
2021-03-22 16:23:26 +0100

    Project initialization with springboot mysql

---
+++ src/main/java/com/spmproject/smartparking/SmartparkingApplication.java
@@ -0,0 +1,13 @@
+package com.spmproject.smartparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartparkingApplication.class, args);
+	}
+
+}
