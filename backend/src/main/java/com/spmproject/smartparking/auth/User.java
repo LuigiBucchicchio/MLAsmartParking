bc4511eed387c7265e0517cf9d70341bfee1c3fe
@miris-mp miris-mp <miriam.petrocchi94@gmail.com>
2021-04-20 08:40:59 +0200

    add react project

--- backend/src/main/java/com/spmproject/smartparking/auth/User.java
+++ backend/src/main/java/com/spmproject/smartparking/auth/User.java
@@ -20,7 +20,7 @@ import static javax.persistence.GenerationType.SEQUENCE;
 @NoArgsConstructor
 @Entity
 @Table(
-        name = "user",
+        name = "users",
         uniqueConstraints = {
                 @UniqueConstraint(name = "user_email_unique", columnNames = "email")
         })
@@ -34,6 +34,7 @@ public class User {
 
     private String name;
 
+    @Column(name = "email")
     @Email(message = "*Please provide a valid Email")
     @NotEmpty(message = "*Please provide an email")
     private String email;
