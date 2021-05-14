3c6c7819ad5c7742d4e7dc6e2788324086925162
Miriam Petrocchi <miriam.petrocchi94@gmail.com>
2021-05-14 08:16:51 +0200

    added layout sign up and fix layout sign in

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -21,7 +21,6 @@ const Login = (props) => {
                     <label for="tab-2" className={classes.tab} >Sign Up</label>
                     <form onSubmit={submitHandler} className={classes.loginForm}>
                         <div className={classes.signInHtm} >
-
                             <div className={classes.group} >
                                 <label htmlFor="name">Name:</label>
                                 <input type="text" name="name" id="name" onChange={e => setDetails} />
@@ -34,7 +33,34 @@ const Login = (props) => {
                                 <label htmlFor="password">Password:</label>
                                 <input type="password" name="password" id="password" />
                             </div>
-                            <input type="submit" value="LOGIN" />
+                            <div className={classes.group}>
+                                <input type="submit" className={classes.button} value="Sign In" />
+                            </div>
+                        </div>
+                        <div className={classes.signUpHtm}>
+                            <div className={classes.group}>
+                                <label for="user" class="label">Username</label>
+                                <input id="user" type="text" class="input" />
+                            </div>
+                            <div className={classes.group}>
+                                <label for="pass" class="label">Password</label>
+                                <input id="pass" type="password" class="input" data-type="password" />
+                            </div>
+                            <div className={classes.group}>
+                                <label for="pass" class="label">Repeat Password</label>
+                                <input id="pass" type="password" class="input" data-type="password" />
+                            </div>
+                            <div className={classes.group}>
+                                <label for="pass" class="label">Email Address</label>
+                                <input id="pass" type="text" class="input" />
+                            </div>
+                            <div className={classes.group}>
+                                <input type="submit" class="button" value="Sign Up" />
+                            </div>
+                            <div class="hr"></div>
+                            <div class="foot-lnk">
+                                <label for="tab-1">Already Member?</label>
+                            </div>
                         </div>
                     </form>
                 </div>
